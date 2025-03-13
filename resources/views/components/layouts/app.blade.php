002127c7960bdb54f535c1dc6373e9c76341af39
Josef Bauer <josef.bauer.1st@gmail.com>
2025-03-13 22:52:57 +0100

    Initial commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}" data-theme="dim">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Welcome</title>
+
+        @vite(["resources/css/app.css", "resources/js/app.js"])
+    </head>
+
+    <body>
+        {{ $slot }}
+    </body>
+</html>
